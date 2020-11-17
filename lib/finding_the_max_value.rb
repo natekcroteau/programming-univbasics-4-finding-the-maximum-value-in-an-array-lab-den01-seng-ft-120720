cc6978a1bb7dc65593e4a99a518b868dc4b2e071
Nate Croteau <natekcroteau@gmail.com>
2020-11-17 23:13:49 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,9 @@
 def find_max_value(array)
    count = 0                                    
  while count < array.length do  
-  puts array[count] == array.max 
+  if array[count] == array.max 
+    return array[count]
+  end 
   count += 1                                    
  end
 end
